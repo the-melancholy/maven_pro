b395222d8c1b43dc714d8d6b51a8652e1beb27e0
jczhang <1498130271@qq.com>
2023-03-24 10:14:54 +0800

    idea first commit

---
+++ javase-maven/src/main/java/com/zjc/HelloMaven.java
@@ -0,0 +1,7 @@
+package com.zjc;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("hello maven");
+    }
+}
---
+++ javase-maven/src/test/java/com/zjc/TestHelloMaven.java
@@ -0,0 +1,7 @@
+package com.zjc;
+
+public class TestHelloMaven {
+    public static void main(String[] args) {
+        System.out.println("hello maven");
+    }
+}
