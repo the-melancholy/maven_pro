98fe4ec31c549c6b8519d0de0328cc03d08222b4
jczhang <1498130271@qq.com>
2023-03-24 10:23:42 +0800

    idea second commit

--- javase-maven/src/main/java/com/zjc/HelloMaven.java
+++ javase-maven/src/main/java/com/zjc/HelloMaven.java
@@ -3,5 +3,6 @@ package com.zjc;
 public class HelloMaven {
     public static void main(String[] args) {
         System.out.println("hello maven");
+        System.out.println("hello maven");
     }
 }
