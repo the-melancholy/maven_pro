7da1816e68955433ea9f2422aac5a38081d5f67d
jczhang <1498130271@qq.com>
2023-03-24 10:37:38 +0800

    hot-fix commit1

--- javase-maven/src/main/java/com/zjc/HelloMaven.java
+++ javase-maven/src/main/java/com/zjc/HelloMaven.java
@@ -4,5 +4,7 @@ public class HelloMaven {
     public static void main(String[] args) {
         System.out.println("hello maven");
         System.out.println("hello maven");
+        System.out.println("this is hot-fix,hello master");
+
     }
 }
